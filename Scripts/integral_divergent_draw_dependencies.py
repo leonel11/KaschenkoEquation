402605f7fc931ff984811a2c0369881ad11ce70b
Leon Unknown <leon19unknown@gmail.com>
2019-06-10 11:31:26 +0300

    Добавлены презентации с результатами по двум краевым задачам

--- Scripts/integral_divergent_draw_dependencies.py
+++ Scripts/integral_divergent_draw_dependencies.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 g_min = -4.0
-g_max = 4.0
+g_max = 1000.0
 h = 0.001
 
 
@@ -18,9 +18,14 @@ def d_func(g):
         return -5.0*g*np.sin(3.0*np.sqrt(g))/(48.0*np.sin(np.sqrt(g))) - 0.75
 
 
+def print_sign_points(gammas, ds):
+    for idx in range(len(ds)-1):
+        if ds[idx]*ds[idx+1] < 0:
+            print(np.mean([gammas[idx], gammas[idx+1]]))
+
+
 def prepare_plot():
     plt.xlabel('$\gamma$')
-    plt.ylabel('$d_0$', rotation='horizontal', position=(0.0, 0.55))
     plt.grid(True)
     plt.subplots_adjust(left=0.08, bottom=0.11, right=0.98, top=0.98)
     plt.axhline(y=0.0, linewidth=2, color='grey', zorder=2)
@@ -29,6 +34,7 @@ def prepare_plot():
 def draw_phid_dependencies(gammas, ds):
     plt.figure('integral_divergent_d0_g[{:.5},{:.5}]'.format(g_min, g_max))
     prepare_plot()
+    plt.ylabel('$d_0$', rotation='horizontal', position=(0.0, 0.55))
     plt.plot(gammas, ds, color='darkorange', linewidth=2, zorder=3)
     # x1,x2,y1,y2 = plt.axis()
     # plt.axis((x1,4.902,y1,y2))
@@ -36,8 +42,9 @@ def draw_phid_dependencies(gammas, ds):
 
 
 def draw_amplitude_dependency(gammas, amps):
-    plt.figure('divergent_A_x0={:.5},beta={:.5}'.format(x0, beta))
+    plt.figure('divergent_A_g[{:.5},{:.5}]'.format(g_min, g_max))
     prepare_plot()
+    plt.ylabel('$A$', rotation='horizontal', position=(0.0, 0.55))
     plt.plot(gammas, amps, label='$A_u$', color='crimson', linewidth=2)
     plt.legend()
     plt.show()
@@ -45,4 +52,7 @@ def draw_amplitude_dependency(gammas, amps):
 
 gammas = list(np.arange(g_min, g_max, h))
 ds = [d_func(g) for g in gammas]
+amps = [np.sqrt(1.0/abs(d)) for d in ds]
+print_sign_points(gammas, ds)
 draw_phid_dependencies(gammas, ds)
+draw_amplitude_dependency(gammas, amps)
